7e9bbe089cd0c67cece9b03fdab0260dd3d83270
RicardoParizotto <ricardo.dparizotto@gmail.com>
2015-08-19 19:10:21 -0300

    interface

--- src/main.c
+++ src/main.c
@@ -1,29 +1,36 @@
 #include "node_config.h"
 #include "haversin.h"
+#include <string.h>
 
 int main ( int arcv, char ** argc ){
 
-	FILE * first_occurency, * taxi_data;
+	FILE * first_occurency, * taxi_data, * out_file;
+	char out_name[200];
+
+
 	char line[200];
-	int map[400], h, aux, n, ctime, select, i, j, day=1;
+	int map[400], h, aux, n, ctime = 0, select, i, j, day=1;
 	int qtd[MAX + 1], ex_min[MAX + 1];
 	double time[MAX + 1], temp;
 	
-	date NOW;
-
 	if(!(first_occurency = malloc(sizeof(FILE))))exit(1);
 	if(!(first_occurency = fopen(argc[1], "r")))exit(1);
 	if(!(taxi_data = malloc(sizeof(FILE)))) exit(1);
 	if(!(taxi_data = fopen(argc[2], "r"))) exit(1);
 
+	printf("0 - Analise da rede como um todo\n1 - Analise da rede com táxis com maior range(alfa)\n2 - Analise diaria da rede\n\n");
+	scanf("%d", &select);
+	printf("Digite o nome do arquivo de saída(max 200 carac.\n");
+	scanf(" %s", out_name);
+	strcat(out_name, ".txt");
+	
+	if(!(out_file = malloc(sizeof(FILE))))exit(1);
+	if(!(out_file = fopen(out_name, "w+")))exit(1);
+
 	memset(qtd, 0, (MAX + 1)*sizeof(int));
 	memset(time, 0, (MAX + 1)*sizeof(double));
 	memset(ex_min, 0, (MAX + 1)*sizeof(int));
-
-	scanf("%d", &select);
-	ctime = 0;
-
-
+		
 	for ( n = 0; n < MAX; n++ ){
 		fscanf( first_occurency, " %[^\n]", line );
 		sscanf( line, "%d", &h );
@@ -42,10 +49,10 @@ int main ( int arcv, char ** argc ){
 	
 	mount();
 
-	
+	//depois de montar, é necessário contabilizar o tamanho dos componentes criados
 	for( h = 0; h < MAX; h++ )
 		if(ch[h] == h)
-			qtd[sz[h]]++;
+			qtd[sz[h]]++;		
 
 
 	while( fscanf( taxi_data, " %d;", &n) != EOF ){
@@ -57,30 +64,37 @@ int main ( int arcv, char ** argc ){
 		for ( aux = 0; line[aux]!= '('; aux++ );
 		sscanf( &line[++aux], "%lf %lf", &grid[map[n]].x, &grid[map[n]].y);
 
+		//Conforme as alterações nas arestas, é feita a reorganização dos componentes
 		change(map[n]);
 		
+		for ( h = 0; h < MAX; h++ ){
+			if(new[h] && ch[h]==h){
+				qtd[sz[h]]++;
+				time[sz[h]]+=difference(BIRTH[h], NOW);
+				BIRTH[h] = NOW;   
+			}
+		}
+
 		if(select == 2){
 			if (NOW.dd != day){
+				fprintf(out_file, "%d/%d/%d\n", NOW.yy, NOW.mm, day);
 				day = NOW.dd;
 				for ( h = 0; h < MAX; h++ )
-					if(ch[h] == h)
-						time[sz[h]]+=difference(BIRTH[sz[h]], NOW);
-	
-				printf("%d/%d/%d\n  ", NOW.yy, NOW.mm, NOW.dd);
-
+					if(ch[h] == h){
+						time[sz[h]]+=difference(BIRTH[h], NOW);
+						if(!qtd[sz[h]]) qtd[sz[h]]++;			//caso o componente não tenha se modificado de um dia para outro. É importante deixar claro que ele existe para contabilizar o tempo.
+					}
+																
+				for( h = 0; h < MAX; h++)
+					BIRTH[h] = NOW;
+			
 				for ( h = 0; h <= MAX; h++ )
-					if(qtd[h]) printf("%d -> %d | media : %lf\n", h, qtd[h], time[h]/qtd[h]);
+					if(qtd[h]) fprintf(out_file, "%d -> %d | media : %lf\n", h, qtd[h], time[h]/qtd[h]);
 				memset(qtd, 0, (MAX + 1)*sizeof(int));
 				memset(time, 0, (MAX + 1)*sizeof(double));
 			}
 		}
 
-		for ( h = 0; h < MAX; h++ )
-			if(new[h] && ch[h]==h){
-				qtd[sz[h]]++;
-				time[sz[h]]+=difference(BIRTH[sz[h]], NOW);
-				BIRTH[sz[h]] = NOW;
-			}
 	
 		if(select == 1){
 			if(ctime++ == 0){
@@ -101,17 +115,16 @@ int main ( int arcv, char ** argc ){
 		case 0:
 				for ( h = 0; h < MAX; h++ )
 					if(ch[h] == h)
-						time[sz[h]]+=difference(BIRTH[sz[h]], NOW);
+						time[sz[h]]+=difference(BIRTH[h], NOW);
 	
 				for ( h = 0; h <= MAX; h++ )
-					if(qtd[h]) printf("%d -> %d | media : %lf\n", h, qtd[h], time[h]/qtd[h]);
+					if(qtd[h]) fprintf(out_file, "%d -> %d | media : %lf\n", h, qtd[h], time[h]/qtd[h]);
 
 				break;
-				
-				
+								
 		case 1: 
 				for ( i = 0; i < MAX; i++ )
-					printf("%d -> %d\n", i, ex_min[i]);
+					fprintf(out_file, "%d -> %d\n", i, ex_min[i]);
 				
 				break;
 
